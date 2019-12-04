3d10abd3eff3c652a60055199425dbf30becb5ed
ArthurYis <shangdishiwode@hotmail.com>
2019-12-04 14:59:25 +0800

    modify Host

--- client/define/Define.go
+++ client/define/Define.go
@@ -2,11 +2,11 @@
  * @Description:
  * @Author: Arthur
  * @Date: 2019-10-09 17:16:59
- * @LastEditTime: 2019-10-09 17:18:29
+ * @LastEditTime: 2019-12-04 14:58:50
  * @LastEditors: Arthur
  */
 package define
 
-const HOST = "127.0.0.1"
+const HOST = "tyto.athena.sspaas.net"
 const GRPC_PORT = ":1233"
 const HTTP_PORT = ":1234"
